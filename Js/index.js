81a62d93c6e7098575c698dd9b40d86a3c8138f7
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-02-04 16:19:59 -0500

    mobile nav done

---
+++ Js/index.js
@@ -0,0 +1,8 @@
+
+const hamburger = document.querySelector('.hamburger')
+const navLinks = document.querySelector('.nav-links')
+hamburger.addEventListener('click', e => {
+    e.currentTarget.classList.toggle('is-active')
+
+    navLinks.classList.toggle('active')
+})
