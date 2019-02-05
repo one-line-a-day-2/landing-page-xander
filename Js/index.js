604c699f31d93641aa44618d30c0fb3152fcaeb0
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-02-05 11:36:56 -0500

    added logo to nav

--- Js/index.js
+++ Js/index.js
@@ -1,9 +1,15 @@
 
 const hamburger = document.querySelector('.hamburger')
 const navLinks = document.querySelector('.nav-links')
+const userCommentsSection = document.querySelector('.user-comments')
 hamburger.addEventListener('click', e => {
     e.currentTarget.classList.toggle('is-active')
 
     navLinks.classList.toggle('active')
-    navLinks.style.zIndex = 9;
+    
+    if(userCommentsSection.style.zIndex >= 0){
+        userCommentsSection.style.zIndex = -1;
+    }else{
+        userCommentsSection.style.zIndex = 0;
+    }
 })
