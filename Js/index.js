2aa234ab0f33472b345404631a45060f17130afd
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-02-07 12:39:01 -0500

    fixed polished styling and added userCounter

--- Js/index.js
+++ Js/index.js
@@ -2,6 +2,8 @@
 const hamburger = document.querySelector('.hamburger')
 const navLinks = document.querySelector('.nav-links')
 const userCommentsSection = document.querySelector('.user-comments')
+const userCount = document.getElementById('user-count');
+
 hamburger.addEventListener('click', e => {
     e.currentTarget.classList.toggle('is-active')
 
@@ -13,3 +15,39 @@ hamburger.addEventListener('click', e => {
         userCommentsSection.style.zIndex = 0;
     }
 })
+
+/**
+ * counting animation
+ */
+function countUP(totalCount){
+    
+    let i = 0
+    
+    const intervalID = setInterval(()=>{
+        if(i <= totalCount){
+            userCount.textContent = i;
+            i ++;
+        }else{
+            clearInterval(intervalID)
+        }
+    }, 100)
+}
+
+/**
+ * Keep the usercount Updated
+ */
+function liveUpdate() {
+    setInterval( () =>{
+        fetch('https://one-line-a-day-2.herokuapp.com/api/users/quantity').then((res)=>{
+            return res.json()
+        }).then((count) => {
+            if(Number(userCount.textContent) != count){
+                countUP(count)
+            }
+        })
+    }, 5000)
+}
+
+window.onload = () => {
+    window.liveUpdate()
+}
